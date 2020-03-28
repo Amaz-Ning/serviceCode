f4ac787c88075c15ab1fd4e3a7bd600e3973e382
Amaz <ningfront@163.com>
2020-03-28 11:56:12 +0800

    修改代码

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
-    entry: './main.js',
-    output: {
-        filename: '[name]-[chunkhash:8].js',
-        path: path.resolve(__dirname, './build')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                use: 'vue-loader'
-            },
-            {
-                test: /\.(png|jpeg|jpg|gif)$/,
-                use: 'url-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html'
-        })
-    ],
-    devServer: {
-        open: true
-    }
+  entry: './main.js',
+  output: {
+    filename: '[name]-[chunkhash:8].js',
+    path: path.resolve(__dirname, './build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        use: 'url-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    open: true
+  }
 }
