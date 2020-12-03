aba1cb1415613fe0c38bee8f3c96f916213ee0fb
wangning <wangning@eqxiu.com>
2020-12-03 14:26:43 +0800

    change path

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 function loginIn(userName, userPassword) {
-    return axios.post('http://172.17.0.8:9001/users/login',{
+    return axios.post('http://42.192.134.212:9001/users/login',{
         userName,
         userPassword
     })
 }
 function register(userName, userPassword) {
-    return axios.post('http://172.17.0.8:9001/users/register', {
+    return axios.post('http://42.192.134.212:9001/users/register', {
         userName,
         userPassword
     })
