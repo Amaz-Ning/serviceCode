87a3a80b4c760231de8d2ce75248d9c0515e3e20
wangning <wangning@eqxiu.com>
2020-12-03 14:34:23 +0800

    修改路径

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 function loginIn(userName, userPassword) {
-    return axios.post('http://42.192.134.212:9001/users/login',{
-        userName,
-        userPassword
+    return axios.get('http://42.192.134.212:9001/index',{
+        // userName,
+        // userPassword
     })
 }
 function register(userName, userPassword) {
